fe610e4db507ecdc598d7adb4f3f88798afa7c41
Kaustubh <ksd.tcet@gmail.com>
2024-11-04 12:02:16 +0530

    #14 updated patern

--- src/main/kotlin/EP4ext/kebabCase.kt
+++ src/main/kotlin/EP4ext/kebabCase.kt
@@ -1,5 +1,5 @@
 fun convertToKebabCase(text: String): String {
-    val lowerCaseText = convertToLowercase(text)
+    val lowerCaseText = convertToLowercaseAndTrimSpace(text)
     return buildString {
         for (char in lowerCaseText ){
             if (char == ' '){
@@ -11,21 +11,39 @@ fun convertToKebabCase(text: String): String {
     }
 }
 
-fun convertToLowercase(text: String): String {
-    return buildString {
-        for (char in text){
-            append(convertToLowercase(char))
+//fun convertToLowercase(text: String): String {
+//    return buildString {
+//        for (char in text){
+//            append(convertToLowercase(char))
+//        }
+//    }
+//}
+//
+//fun convertToLowercase(char: Char): Char {
+//    return when(char){
+//        in 'A'..'Z' -> char.lowercaseChar()
+//        else -> char
+//    }
+//}
+
+fun convertToLowercaseAndTrimSpace(text: String):String {
+    var result = ""
+    for(char in text){
+        if(char in 'A'..'Z'){
+            result += (char + 32)
+        }
+        else if(char == ' ') {
+            if(result.last() != ' ') {
+                result += char
+            }
+        }
+        else {
+            result += char
         }
     }
-}
-
-fun convertToLowercase(char: Char): Char {
-    return when(char){
-        in 'A'..'Z' -> char.lowercaseChar()
-        else -> char
-    }
+    return result
 }
 
 fun main(){
-    print(convertToKebabCase("This is KebabCase"))
+    print(convertToKebabCase("This  is       465    KebabCase"))
 }
