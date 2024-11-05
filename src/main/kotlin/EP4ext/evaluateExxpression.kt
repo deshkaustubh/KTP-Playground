27e7c9b6b9adb209457c98144d38fddaf46152fe
Kaustubh <ksd.tcet@gmail.com>
2024-11-05 20:12:14 +0530

    initial commit

--- src/main/kotlin/EP4ext/evaluateExxpression.kt
+++ src/main/kotlin/EP4ext/evaluateExxpression.kt
@@ -2,22 +2,33 @@ package org.example.EP4ext
 
 import parseInt
 
-fun main(){
-    println(evaluate("1+2"))
+fun main (){
+    println(evaluateExpression("-1+2+3-5+200"))
 }
 
-fun evaluate(expression: String) : MutableList<Char> {
-    var plusList = mutableListOf<String>()
-    var minusList = mutableListOf<String>()
-    var indiceList = mutableListOf<Char>()
-    var string = ""
+fun evaluateExpression(exp: String): Int{
+    var res = 0
+    var isNeg = false
+    var num = ""
 
-   for ( i in 0 until expression.length){
-       if(expression[i] in listOf('-','+')){
-           var j = i + '0'
-           indiceList.add(i + '0')
-           break
-       }
-   }
-    return  indiceList
+    for(i in 0..exp.length) {
+        if(i == exp.length || exp[i] in listOf('+','-')) {
+
+            if (num.isNotEmpty()) {
+                var parsedNum = parseInt(num)
+                if (isNeg) {
+                    parsedNum *= -1
+                    isNeg = false
+                }
+                res += parsedNum
+                num = ""
+            }
+
+            if (i < exp.length && exp[i] == '-') isNeg = true
+        }
+        else {
+            num += exp[i]
+        }
+    }
+    return res
 }
