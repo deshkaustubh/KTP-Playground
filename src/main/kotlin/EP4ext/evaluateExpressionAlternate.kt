1a51993486f2c4bbaa56899309382edf98d7b77b
Kaustubh <ksd.tcet@gmail.com>
2024-11-07 22:14:33 +0530

    updated pattern

--- src/main/kotlin/EP4ext/evaluateExpressionAlternate.kt
+++ src/main/kotlin/EP4ext/evaluateExpressionAlternate.kt
@@ -3,16 +3,17 @@ package org.example.EP4ext
 import java.util.Scanner
 
 fun main() {
-    val input = "-1+2-3"
-    val scanner = Scanner(input.replace("+", " +").replace("-", " -"))
+    val expression = "-1+2-3"
+    //here in scanner I could have used the substring function by changing char type of char and replacement to String
+    val addSpaceToSign = Scanner(expression.replace("+", " +").replace("-", " -"))
 
     val positiveNumbers = mutableListOf<Int>()
     val negativeNumbers = mutableListOf<Int>()
 
-    while (scanner.hasNext()) { // here hasNext is for running the while loop until it has next element
-        val number = scanner.nextInt() // this takes the next input after space as Int
+    while (addSpaceToSign.hasNext()) { // here hasNext is for running the while loop until it has next element
+        val number = addSpaceToSign.nextInt() // this takes the next input after space as Int
         if (number > 0) {
-            positiveNumbers.add(number)
+            positiveNumbers.add(number) // add == append
         } else {
             negativeNumbers.add(number)
         }
