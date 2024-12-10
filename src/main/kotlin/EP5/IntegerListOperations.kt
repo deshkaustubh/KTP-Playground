09b3e67f2e26b31f3c5f911a6494171028eda616
Kaustubh <ksd.tcet@gmail.com>
2024-12-10 22:45:13 +0530

    initial commit

--- src/main/kotlin/EP5/IntegerListOperations.kt
+++ src/main/kotlin/EP5/IntegerListOperations.kt
@@ -1,2 +1,7 @@
 package EP5
 
+fun elementAt(list: List<Int>,index: Int) : Int? {
+    if ( index < 0 || index > list.lastIndex) return null
+    return list[index]  // in such cases IndexOutOfBoundException is possible so null is used.
+}
+
